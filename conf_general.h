a4ae1bae189b9ccbfec1e6a1bf09558a88e72815
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2015-02-21 15:30:42 +0100

    HW_VERSION set to 4.0

--- conf_general.h
+++ conf_general.h
@@ -47,8 +47,8 @@
  * Select only one hardware version
  */
 //#define HW_VERSION_BW
-//#define HW_VERSION_40
-#define HW_VERSION_45
+#define HW_VERSION_40
+//#define HW_VERSION_45
 //#define HW_VERSION_46
 //#define HW_VERSION_R2
 //#define HW_VERSION_VICTOR_R1A
